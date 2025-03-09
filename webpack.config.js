f59abd16702817dd271c1c2c9d4fb55e8d72524e
thromer <tromer@gmail.com>
2025-03-09 11:30:10 -0700

    Prep for packaging (#7)

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: 'commonjs2'
+    },
+    clean: true,
+    sourceMapFilename: '[file].map'
+  },
+  optimization: {
+    minimize: true
+  }
+};
